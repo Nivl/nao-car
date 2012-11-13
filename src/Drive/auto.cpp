9d2585261894817ed1ac42544d5276ffaaf450fd
loick michard <michar_l@pc-michar_l.(none)>
2012-11-13 18:16:00 +0100

    Line following

--- src/Drive/auto.cpp
+++ src/Drive/auto.cpp
@@ -5,7 +5,7 @@
 // Login   <jochau_g@epitech.net>
 // 
 // Started on  Fri Sep 28 13:59:38 2012 gael jochaud-du-plessix
-// Last update Mon Nov 12 16:36:10 2012 samuel olivier
+// Last update Tue Nov 13 17:59:37 2012 loick michard
 //
 
 #include <alcommon/almodulecore.h>
@@ -22,6 +22,7 @@
 #include <signal.h>
 
 #include "DriveProxy.hpp"
+#include "time.h"
 
 using namespace cv;
 
@@ -67,7 +68,83 @@ bool isCollapsed(Vec4i& line1, Vec4i& line2, double threshold=10.0);
 double lineSpace(Vec4i& line1, Vec4i& line2);
 double distance(double x1, double y1, double x2, double y2);
 
+typedef enum State {
+  Up,
+  Down,
+  Stop
+} State;
+
+typedef enum Direction {
+  Right,
+  Left,
+  Front
+} Direction;
+
 DriveProxy	*proxy;
+
+#define VALUE_RIGHT 0.55
+#define VALUE_LEFT 0.45
+#define VALUE_TOO_RIGHT 0.85
+#define VALUE_TOO_LEFT 0.15
+
+void	getNextState(State &state, Direction &direction, double value)
+{  
+  static bool isStart = false;
+  static clock_t start;
+  
+  if (state == Stop)
+    state = Up;
+  else if (state == Up)
+    {
+      if (direction == Right)
+	{
+	  if (value < VALUE_RIGHT && value > VALUE_LEFT)
+	    direction = Front;
+	  if (value <= VALUE_LEFT || value >= VALUE_TOO_RIGHT)
+	    {
+	      direction = Left;
+	      state = Down;
+	    }
+	}
+      else if (direction == Left)
+	{
+          if (value < VALUE_RIGHT && value > VALUE_LEFT)
+            direction = Front;
+          if (value <= VALUE_TOO_LEFT || value >= VALUE_RIGHT)
+            {
+	      direction = Right;
+              state = Down;
+            }
+        }
+      else
+	{
+	  if (value > VALUE_RIGHT)
+	    direction = Right;
+	  else if (value < VALUE_LEFT)
+	    direction = Left;
+	}
+    }
+  else
+    {
+      if (!isStart)
+	{
+	  proxy->stopPush();
+	  isStart = true;
+	  start = clock();
+	}
+      else
+	{
+	  clock_t end = clock();
+	  if (((double)end - start) / CLOCKS_PER_SEC > 1.0)
+	    {
+	      isStart = false;
+	      proxy->stopPush();
+	      direction = Front;
+	      state = Up;
+	    }
+	}
+    }
+}
 
 void	catchSig(int signum)
 {
@@ -120,11 +197,11 @@ int main(int ac, char **av)
   namedWindow("opencv2", CV_WINDOW_AUTOSIZE | CV_WINDOW_FREERATIO);
   cvMoveWindow("opencv2", 640, 0);
 
-  int tolerance = 200;
+  int tolerance = 210;//223;
   createTrackbar("Tolerance", "opencv", &tolerance, 255);
-  int untolerance = 80;
+  int untolerance = 174;//171;
   createTrackbar("Untolerance", "opencv", &untolerance, 255);
-  int v1 = 4, v2 = 80, v3 = 80, v4 = 30, v5 = 50;
+  int v1 = 4, v2 = 80, v3 = 80, v4 = 37, v5 = 9;
   createTrackbar("v1", "opencv", &v1, 200);
   createTrackbar("v2", "opencv", &v2, 200);
   createTrackbar("v3", "opencv", &v3, 200);
@@ -134,18 +211,24 @@ int main(int ac, char **av)
   Mat mask;
   Vec4i redLine(0, 0, 0, 0);
 
-  double objectivesStartY = 200;
-  double objectivesEndY = 480;
-  double objectivesStep = 2;
+  double objectivesStartY = 150 * src.rows / 480;
+  double objectivesEndY = src.rows;
+  double objectivesStep = 1;
   
   vector<Vec2i> points;
   for (size_t k = 0; k < (objectivesEndY - objectivesStartY) / objectivesStep; ++k)
     {
-      Vec2i point(320, objectivesEndY - objectivesStep * k);
+      Vec2i point(320 * src.cols / 640, objectivesEndY - objectivesStep * k);
       points.push_back(point);
     }
 
+  //int previousStatus = 0;
+  //int previousMotion = 0;
+  State state = Stop;
+  Direction direction = Front;
+  double values[5] = {-1, -1, -1, -1, -1};
   do {
+
     if (!src.data)
       continue ;
 
@@ -182,7 +265,7 @@ int main(int ac, char **av)
 	bool found = false;
 	if (1){
 	for (size_t j = 0; j < collapsedLines.size() && !found; ++j) {
-	  if (isCollapsed(lines[i], collapsedLines[j], 50)) {
+	  if (isCollapsed(lines[i], collapsedLines[j], 50 * src.rows / 640)) {
 	    double max = -1;
 	    for (int k = 0; k < 4; k++)
 	      {
@@ -228,7 +311,7 @@ int main(int ac, char **av)
 	  }
 	}
 	// std::cout << "Choosed nearest\n";
-	if (std::abs(nearest[1] - nearest[3]) > 50) {
+	if (std::abs(nearest[1] - nearest[3]) > 50 * src.cols / 640) {
 	  redLine = nearest;
 	  found = true;
 	}
@@ -236,7 +319,7 @@ int main(int ac, char **av)
     // If we don't have a line, take the first of the list
     if (!found && collapsedLines.size() > 0) {
       // std::cout << "Choosed first\n";
-      if (std::abs(collapsedLines[0][1] - collapsedLines[0][3]) > 50)
+      if (std::abs(collapsedLines[0][1] - collapsedLines[0][3]) > 50 * src.cols / 640)
 	redLine = collapsedLines[0];
     }
     // Find objectives points
@@ -244,7 +327,7 @@ int main(int ac, char **av)
 
     for (size_t y = objectivesEndY; y > objectivesStartY; y -= objectivesStep)
       {
-	double prevX = 320;
+	double prevX = 320 * src.cols / 640;
 	if (index > 0)
 	  prevX = points[index - 1][0];
 	double distanceToPrevX = -1;
@@ -257,40 +340,41 @@ int main(int ac, char **av)
 		  double k = (double)(collapsedLines[i][3] - collapsedLines[i][1]) / (collapsedLines[i][2] - collapsedLines[i][0]);
 		  double b = collapsedLines[i][3] - k * collapsedLines[i][2];
 		  double x = (y - b) / k;
-		  if  (distanceToPrevX == -1 || abs(x - prevX) + abs(x - 320) < distanceToPrevX)
+		  if  (distanceToPrevX == -1 || abs(x - prevX) + abs(x -  320 * src.cols / 640) < distanceToPrevX)
 		    {
-		      distanceToPrevX = abs(x - prevX) + abs(x - 320);
+		      distanceToPrevX = abs(x - prevX) + abs(x - 320 * src.cols / 640);
 		      newX = x;
 		    }
 		}
 	    }
 	}
-	if (newX != -1)
-	  {
-	    if (points[index][0] - newX > 5)
-	      points[index][0] -= 5;
-	    else if (points[index][0] - newX < -5)
-	      points[index][0] += 5;
-	    //else
-	    //points[index][0] = newX;
-	  }
-	else if (index > 1)
-	  {
-	    if (points[index - 1][1] != collapsedLines[index - 2][1] && 0)
-	      {
-		double dx = (double)(points[index - 1][0] - points[index - 2][0]) / (points[index - 1][1] - collapsedLines[index - 2][1]);
-		Vec2i point(points[index - 1][0] + dx * objectivesStep, y);
-		points[index] = point;
-	      }
-	  }
+	points[index][0] = newX;
+	// if (newX != -1)
+	//   {
+	//     if (points[index][0] - newX > 5)
+	//       points[index][0] -= 5;
+	//     else if (points[index][0] - newX < -5)
+	//       points[index][0] += 5;
+	//     //else
+	//     points[index][0] = newX;
+	//   }
+	// else if (index > 1)
+	//   {
+	//     if (points[index - 1][1] != collapsedLines[index - 2][1] && 0)
+	//       {
+	// 	double dx = (double)(points[index - 1][0] - points[index - 2][0]) / (points[index - 1][1] - collapsedLines[index - 2][1]);
+	// 	Vec2i point(points[index - 1][0] + dx * objectivesStep, y);
+	// 	points[index] = point;
+	//       }
+	//   }
 	index += 1;
       }
 
-    // //Debug lines
-    // for (int i = 0; i < collapsedLines.size(); ++i) {
-    //   line(color_dst, Point(collapsedLines[i][0], collapsedLines[i][1]),
-    // 	   Point(collapsedLines[i][2], collapsedLines[i][3]), Scalar(255, 0, 0), 3, 8);
-    // }
+    //Debug lines
+    for (int i = 0; i < collapsedLines.size(); ++i) {
+      line(color_dst, Point(collapsedLines[i][0], collapsedLines[i][1]),
+    	   Point(collapsedLines[i][2], collapsedLines[i][3]), Scalar(0, 0, 255), 3, 8);
+    }
 
     // LA OU CA MARCHE PAS MAIS IL FAUT LENLEVER ET LA FAIRE MARCHER AVANT, CA DOIT AFFICHER UNE PETITE LIGNE ROUGE(COMME TA BITE)
     // line(color_dst, Point(0, 0), Point(50, 50), Scalar(255, 0, 0), 3, 8);
@@ -303,49 +387,162 @@ int main(int ac, char **av)
     //	 Point(redLine[2], redLine[3]), Scalar(0, 255, 0), 3, 8);
 
     vector<double> roadCoords;
-    roadCoords.push_back(280);
-    roadCoords.push_back(310);
-    roadCoords.push_back(350);
-    roadCoords.push_back(380);
-    roadCoords.push_back(400);
+    for (int value = 380; value < 440; value += 15)
+      roadCoords.push_back(value * src.rows / 480);
+    // roadCoords.push_back(180 * src.rows / 480);
+    // roadCoords.push_back(200 * src.rows / 480);
+    // roadCoords.push_back(225 * src.rows / 480);
+    // roadCoords.push_back(250 * src.rows / 480);
+    // roadCoords.push_back(280 * src.rows / 480);
+    // roadCoords.push_back(310 * src.rows / 480);
+    // roadCoords.push_back(340 * src.rows / 480);
+    // roadCoords.push_back(370 * src.rows / 480);
+    // roadCoords.push_back(400 * src.rows / 480);
     vector<Vec2i> road;
     for (int i = 0; i < points.size() - 1; ++i) {
       for (int k = 0; k < roadCoords.size(); ++k)
 	{
-	  if (points[i][1] == roadCoords[k])
-	    road.push_back(points[i]);
+	  if (points[i][1] == roadCoords[k] && points[i][0] != -1)
+	    {
+	      road.push_back(points[i]);
+	    }
 	}
     }
 
-    for (int i = 0; i < road.size() - 1; ++i) {
+    for (int i = 0; road.size() != 0 && i < road.size() - 1; ++i) {
       line(color_dst, road[i],
            road[i + 1], Scalar(255, 0, 0), 3, 8);
+      line(color_dst, Point(0, road[i][1]),
+           Point(src.cols, road[i][1]), Scalar(0, 255 - i * 10, 0), 1, 1);
     }
-
+    if (road.size() != 0)
+      {
+	line(color_dst, Point(0, road[road.size() - 1][1]),
+	     Point(src.cols, road[road.size() - 1][1]), Scalar(0, 255 - (road.size() - 1) * 10, 0), 1, 1);
+      }
     double sum = 0.0;
-    for (int i = 0; i < road.size(); ++i) {
-      sum += road[i][0];
+    double div = 0.0;
+    for (int i = 0; road.size() != 0 && i < road.size(); ++i) {
+      sum += (road.size() - i) * road[i][0];
+      div += (road.size() - i);
     }
-    sum /= road.size();
+    if (div > 0)
+      sum /= div;
 
-    if (sum > 340)
-      {
-	proxy->right();
-	std::cout << "RIGHT" << std::endl;
-      }
-    else if (sum < 300)
-      {
-	proxy->left();
-	std::cout << "LEFT" << std::endl;
-      }
+    for (int i = 5; i > 0; --i)
+      values[i] = values[i - 1];
+    values[0] = sum / src.cols;
+    double total = 0;
+    div = 0;
+    for (int i = 0; i < 5; ++i)
+      if (values[i] > 0)
+    	{
+	  std::cout << values[i] << std::endl;
+    	  total += values[i];
+    	  div += 1;
+    	}
+    if (div >= 1)
+      total /= div;
+
+    if (total > 0)
+      getNextState(state, direction, total);
+    std::cout << "TOTAL " << total << std::endl;
+    std::cout << state << std::endl;
+    std::cout << direction << std::endl;
+
+    if (direction == Right)
+      proxy->right();
+    else if (direction == Left)
+      proxy->left();
     else
-      {
-	proxy->stopTurn();
-	std::cout << "FRONT" << std::endl;
-      }
-    // proxy->up();
-    line(color_dst, Point(sum, 0),
-    	 Point(sum, 480), Scalar(255, 255, 255), 3, 8);
+      proxy->stopTurn();
+    if (state == Up)
+      proxy->up();
+    else if (state == Down)
+      proxy->down();
+    else
+      proxy->stopPush();
+
+    // bool recule = false;
+    // if (sum == 0) {}
+    // else if (sum > 450 * src.cols / 640 && (previousStatus == 1 || previousStatus == 0))
+    //   {
+    // 	proxy->left();
+    // 	std::cout << "LEFT" << std::endl;
+    // 	recule = true;
+    //   }
+    // else if (sum < 190 * src.cols / 640 && (previousStatus == 2 || previousStatus == 0))
+    //   {
+    // 	proxy->right();
+    // 	std::cout << "RIGHT" << std::endl;
+    // 	recule = true;
+    //   }
+    // else if (sum > 360 * src.cols / 640 && (previousStatus == 1 || previousStatus == 0))
+    //   {
+    // 	proxy->right();
+    // 	std::cout << "RIGHT" << std::endl;
+    // 	previousStatus = 1;
+    //   }
+    // else if (sum < 280 * src.cols / 640 && (previousStatus == 2 || previousStatus == 0))
+    //   {
+    // 	proxy->left();
+    // 	std::cout << "LEFT" << std::endl;
+    // 	previousStatus = 2;
+    //   }
+    // else if (sum < 360 * src.cols / 640 && sum > 280 * src.cols / 640 )
+    //   {
+    // 	proxy->stopTurn();
+    // 	std::cout << "FRONT" << std::endl;
+    // 	previousStatus = 0;
+    //   }
+    // else
+    //   {
+    // 	if (previousStatus == 1)
+    // 	  {
+    // 	    proxy->left();
+    // 	    recule = true;
+    // 	    previousStatus = 0;
+    // 	  }
+    // 	if (previousStatus == 2)
+    // 	  {
+    // 	    proxy->right();
+    // 	    recule = true;
+    // 	    previousStatus = 0;
+    // 	  }
+    // 	else
+    // 	  proxy->stopTurn();
+    //   }
+    // if ((recule && (previousMotion >= 0 || previousMotion == -100)) || (previousMotion >= 1 && previousMotion != 100))
+    //   {
+    // 	std::cout << "RECULE" << std::endl;
+    // 	if (previousMotion > 90)
+    // 	  previousMotion = 100;
+    // 	else if (previousMotion <= 0)
+    // 	  {
+    // 	    proxy->stopPush();
+    // 	    previousMotion = 1;
+    // 	  }
+    // 	else
+    // 	  ++previousMotion;
+    // 	//proxy->down();
+    //   }
+    // else
+    //   {
+    // 	std::cout << "TOUT DROIT" << std::endl;
+    // 	if (previousMotion < -60)
+    // 	  previousMotion = -100;
+    // 	else if (previousMotion >= 0)
+    // 	  {
+    // 	    proxy->stopPush();
+    // 	    previousMotion = -1;
+    // 	  }
+    // 	else
+    // 	  --previousMotion;
+    // 	//proxy->up();
+    //   }
+    if (total > 0)
+      line(color_dst, Point(total * src.cols, 0),
+	   Point(total * src.cols, src.rows), Scalar(255, 255, 255), 3, 8);
 
     imshow("opencv", color_dst);
     imshow("opencv2", mask);
