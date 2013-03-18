6bd365b18b609a0f6562b3ed11b40931533439d8
Gael du Plessix <gael.duplessix@gmail.com>
2013-03-19 00:27:06 +0100

    Added voice speeker to drive module

---
+++ NaoCar/Modules/Drive/VoiceSpeaker.cpp
@@ -0,0 +1,37 @@
+
+#include "VoiceSpeaker.hpp"
+
+VoiceSpeaker::VoiceSpeaker(boost::shared_ptr<AL::ALBroker>& broker) : 
+  _t2p(broker), _stop(false) {
+  _thread = new std::thread(&VoiceSpeaker::loop, this);
+}
+
+VoiceSpeaker::~VoiceSpeaker() {
+  if (_thread) {
+    _stop = true;
+    _thread->join();
+  }
+}
+
+void VoiceSpeaker::loop() {
+  while (!_stop) {    
+    _mutex.lock();
+    if (_messages.size() > 0) {
+      std::pair<std::string, std::string> message = _messages.front();
+      _messages.pop_front();
+      _mutex.unlock();
+      _t2p.setLanguage(message.second);
+      _t2p.say(message.first);
+    }
+    else {
+      _mutex.unlock();
+      usleep(50000);
+    }
+  }
+}
+
+void VoiceSpeaker::say(const std::string& message, const std::string& language) {
+  _mutex.lock();
+  _messages.push_back(std::pair<std::string, std::string>(message, language));
+  _mutex.unlock();
+}
